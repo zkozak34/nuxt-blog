bb580824f75ee14b39ae33c40aebe061c70f6a8f
Zeynel <zeynell.kozak@gmail.com>
2023-09-21 23:21:22 +0300

    feat(App): initalize repository

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+  <ContentDoc />
+</template>
+
+<script setup lang="ts">
+// imports
+
+// interfaces & types
+
+// constants
+
+// composable
+
+// props
+
+// defineEmits
+
+// states (refs and reactives)
+
+// computed
+
+// watchers
+
+// lifecycles
+
+// methods
+</script>
+
+<style lang="postcss" scoped></style>
